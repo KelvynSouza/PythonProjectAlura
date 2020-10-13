ff913728aedc9bec764330f10e8d4bf4407e9543
Kelvyn <KelvynSouza>
2020-10-13 18:17:26 -0300

    Forca

--- code/forca.py
+++ code/forca.py
@@ -3,29 +3,42 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
     #lista
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    letras_acertadas = ["_" for letra in palavra_secreta]
+
+    #for letra in palavra_secreta:
+    #   letras_acertadas.append("_")
 
     enforcou = False
     acertou = False
+    erros = 0
 
     print(letras_acertadas)
 
     while(not acertou and not enforcou): 
 
         chute = input("Qual letra? ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        index = 0
-        for letra in palavra_secreta:
-            if (chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra
-            index = index + 1
+        if (chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if (chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+            print("Ops, você errou! Faltam {} tentativas.".format(6-erros))
 
-        print(letras_acertadas)
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas) 
 
-        print("Jogando")
+    if (acertou):
+        print("Você ganhou!")
+    else:
+        print("Você perdeu!")
 
     print("Fim do jogo")
 
